a8c973aec8283e4bdb6a6b76e601fdc1e2551e0d
Mohammad Robaitul Islam Bhuiyan <robayetcuet11@gmail.com>
2023-11-13 12:49:30 +0100

    Setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_Homework_2',
+    version='0.1',
+    description='Data Science Survival Skill',
+    author='Robayet',
+    packages=find_packages(exclude=['testt']),
+    zip_safe=False
+)
